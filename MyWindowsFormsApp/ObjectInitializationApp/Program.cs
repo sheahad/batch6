8834bce18e744487c5f70abdac9cd8280a41fa04
MD. KHALEED AL-AMRAN <sheahad@gmail.com>
2019-10-29 09:22:58 +0600

    ObjectinitializerApp Added

---
+++ MyWindowsFormsApp/ObjectInitializationApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectInitializationApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initialize Object
+
+            //Using Object
+            Employee employee = new Employee();
+
+            employee.Id = 1;
+            employee.Name = "Ali";
+            employee.Address = "Mirpur";
+
+            Console.WriteLine("Id: "+ employee.Id + " Name: " + employee.Name + " Address: "+ employee.Address) ;
+
+            //Using Contructor
+            Employee employee2 = new Employee(2, "Mehedi", "Mogbazar");
+            Console.WriteLine("Id: " + employee2.Id + " Name: " + employee2.Name + " Address: " + employee2.Address);
+
+            //Initializer
+            Employee employee3 = new Employee()
+            {
+                Id = 3,
+                Name = "Milon",
+                Address = "Mirpur"
+            };
+            Console.WriteLine("Id: " + employee3.Id + " Name: " + employee3.Name + " Address: " + employee3.Address);
+
+
+            Console.ReadKey();
+        }
+    }
+}
